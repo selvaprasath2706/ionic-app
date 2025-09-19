5e5c73be8e169df5c3125c858e9f40b78f704c68
selvaprasath2706 <selva.prasath2706@gmail.com>
2025-09-20 00:38:52 +0530

    Added the camera support to the app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,46 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonFabButton,
+  IonIcon,
+  IonFab,
+  IonImg,
+  IonCol,
+  IonGrid,
+  IonRow,
+} from '@ionic/angular/standalone';
+
+import { PhotoService } from '../services/photo';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonFabButton,
+    IonIcon,
+    IonFab,
+    IonImg,
+    IonCol,
+    IonGrid,
+    IonRow,
+  ],
 })
 export class Tab2Page {
+  constructor(public photoService: PhotoService) {}
 
-  constructor() {}
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 
+  async ngOnInit() {
+  await this.photoService.loadSaved();
+}
 }
